687d09e1d89a815fca1aa3844f8a4d71c1f97634
Walter McDonald <wemcdona@gmail.com>
2023-05-07 19:21:01 -0500

    const exercise

---
+++ let-const-exercise.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+
+// What is the difference between var and let? let can be changed, but not rededicated like var can
+// What is the difference between var and const? const cannot be changed or rededicated while var can
+// What is the difference between let and const? let can be changed while const cannot
+// What is hoisting? hoisting is what will allow variables to only work for the functions which they are placed in
